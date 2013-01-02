4830ee9229699a386a2243c06f076305391bf396
Endre Karlson <endre.karlson@gmail.com>
2013-01-02 09:33:44 +0000

    Add local sample repos

--- pulp_deb_common/pulp_deb/common/samples.py
+++ pulp_deb_common/pulp_deb/common/samples.py
@@ -1,3 +1,6 @@
+import os
+
+
 BASE_URL = 'http://ubuntu.uib.no/archive'
 
 DIST = 'precise'
@@ -17,9 +20,33 @@ SOURCES_URL = BASE_URL + '/dists/%(dist)s/%(component)s/source/Sources.gz'
 CONTENTS_URL = BASE_URL + '/dists/%(dist)s/Contents-%(arch)s.gz'
 
 
-def get_resource_data(rn):
-    resource = REPO.copy()
+def get_repo(**kw):
+    repo = REPO.copy()
+    repo.update(kw)
+    return repo
+
+
+def get_resource_data(rn, repo_kw={}, **kw):
+    resource = get_repo(**repo_kw)
     resource['component'] = resource.pop('component')[0]
-    resource['arch'] = resource['arch'][0]
+    resource['architecture'] = resource['architecture'][0]
     resource['resource_name'] = rn
     return resource
+
+
+def local_repo_location():
+    return 'file://' + os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', '..', 'test', 'repos')
+
+
+def valid_repo(**kw):
+    if 'url' not in kw:
+        kw['url'] = local_repo_location() + '/valid'
+    repo = get_repo(**kw)
+    return repo
+
+
+def invalid_repo(**kw):
+    if 'url' not in kw:
+        kw['url'] = local_repo_location() + '/invalid'
+    repo = get_repo(**kw)
+    return repo
