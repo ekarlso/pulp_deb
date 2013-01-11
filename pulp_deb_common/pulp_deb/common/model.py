6ca8b50ea7fd02ecdde9149c9da49fd8b12bb6a1
Endre Karlson <endre.karlson@gmail.com>
2013-01-11 09:54:57 +0000

    New models and tests

--- pulp_deb_common/pulp_deb/common/model.py
+++ pulp_deb_common/pulp_deb/common/model.py
@@ -1,51 +1,199 @@
-import codecs
 import copy
-from debian.deb822 import Packages
+from debian.deb822 import Packages, Sources
 import gzip
+import re
 
 from pulp.common.compat import json
-
 from pulp_deb.common import constants
 
 
 UNIT_KEYS = ['package', 'version', 'maintainer']
 
 
-class Repository(object):
+RESOURCE_TYPES = {
+    'sources': Sources,
+    'packages': Packages
+}
 
-    def __init__(self):
-        self.packages = []
 
-    def _read(self, f):
-        if not type(f) == file and type(f) == str:
-            fh = open(f)
-            if f.endswith('.gz'):
-                fh = gzip.GzipFile(fileobj=fh)
-        elif type(f) == file:
-            fh = f
-        else:
-            raise RuntimeError('Need to pass either a path or a file')
-        return fh.readlines()
+def _read(self, f):
+    if not type(f) == file and type(f) == str:
+        fh = open(f)
+    if f.endswith('.gz'):
+        fh = gzip.GzipFile(fileobj=fh)
+    elif type(f) == file:
+        fh = f
+    else:
+        raise RuntimeError('Need to pass either a path or a file')
+    return fh.readlines()
 
-    def _update(self, data, **kw):
-        for pkg in data:
-            pkg.update(kw)
 
-            deb = DebianPackage(pkg)
-            self.packages.append(deb)
+class Model(object):
+    def __init__(self, **kw):
+        self.data = kw
 
-    def update_from_packages(self, packages_file, **kw):
+    def update(self, data):
+        """
+        Updates the instance variables with the values in the given dict.
+        """
+        self.data.update(data)
+
+    def keys(self):
+        return self.data.keys()
+
+    def items(self):
+        return self.data.items()
+
+    def __iter__(self):
+        return iter(self.data)
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+    def __contains__(self, key):
+        return key.lower() in self.data
+
+    @staticmethod
+    def lowered_key(key):
+        return key.lower()
+
+    @staticmethod
+    def uppered_key(key):
+        return '-'.join([i.title() for i in key.split("-")])
+
+    @classmethod
+    def from_dict(cls, data):
+        """
+        Parses the given snippet of model data into an object
+        representation. This call assumes the JSON has already been parsed
+
+        :return: object representation of the given data
+        :rtype:  Model
+        """
+        return cls(data)
+
+    def to_dict(self, exclude=[], **kw):
+        data = self.data.copy()
+        data = dict([(k, v) for k, v in data.items() if not k in exclude])
+        data.update(kw)
+        return data
+
+    def serialize(self, exclude=[], **kw):
+        """
+        Serializes a Model
+
+        :return: dict repr of the Model
+        :rtype: dict
+        """
+        data = self.to_dict(**kw)
+
+        def _dictable(obj):
+            if hasattr(obj, 'to_dict'):
+                print "DICTABLE", obj
+                return True
+            else:
+                return False
+
+        def _list(values):
+            data = []
+            for obj in values:
+                i = _dict(obj)
+                if type(i) == dict:
+                    i = _dict(i)
+                data.append(i)
+            return data
+
+        def _dict(values):
+            if type(values) in (list, set):
+                data = _list(values)
+            elif type(values) == dict:
+                data = dict([(k, _dict(v)) for k, v in values.items()])
+            else:
+                if _dictable(values):
+                    data = values.to_dict(exclude=exclude)
+                else:
+                    data = values
+            return data
+
+        return _dict(data)
+
+    def update_from_json(self, json_str):
+        data = json.loads(json_str)
+        self.update_from_dict(data)
+
+    def to_json(self, **kw):
+        return json.dumps(self.serialize())
+
+
+class Distribution(Model):
+    """
+    A distribution - typically in pulp sense a repo
+    """
+    def __init__(self, **kw):
+        components = list()
+        for values in kw.get('components', {}):
+            cmpt = Component(
+                dist=self,
+                **values)
+            if values['name'] in components:
+                raise ValueError('Multiple components with the same name is now allowed')
+            components.append(cmpt)
+        kw['components'] = components
+        super(Distribution, self).__init__(**kw)
+
+    def get_component(self, name):
+        for c in self.data['components']:
+            if c['name'] == name:
+                return c
+
+    def add_package(self, component_name, package):
+        component = self.get_component(component_name)
+        component.add_package(package)
+
+    def add_packages(self, component_name, packages):
+        for pkg in packages:
+            self.add_package(component_name, pkg)
+
+
+
+class Component(Model):
+    """
+    The Component sits under a repository holding the packages
+    """
+    def __init__(self, dist=None, **kw):
+        self.dist = dist
+        super(Component, self).__init__(**kw)
+        self.data['packages'] = []
+
+    def add_package(self, package):
+        """
+        Adds a package to this Component
+        """
+        self.data['packages'].append(Package(**package))
+
+    def add_packages(self, packages):
+        for p in packages:
+            self.add_package(p)
+
+    def update_from_index(self, index, **kw):
         """
         Updates this instance with packages in the given Packages file.
 
         :return: object representing the repository and all it's packages
         :rtype: Repository
         """
-        try:
-            lines = self._read(packages_file)
-        except RuntimeError:
-            raise
-        self._update(Packages.iter_paragraphs(lines), **kw)
+        lines = _read(index)
+        if re.match(index, 'Sources'):
+            data = Sources.iter_paragraphs(lines)
+        elif re.match(index, 'Packages'):
+            data = Packages.iter_paragraphs(lines)
+        else:
+            raise RuntimeError('Must be either Sources or Packages')
+
+        self.add_packages(data)
 
     def update_from_resources(self, resources):
         for resource in resources:
@@ -65,51 +213,18 @@ class Repository(object):
         parsed = json.loads(json_string)
         self._update(parsed, **kw)
 
-    def to_json(self):
-        """
-        Serialize this repo to JSON
-        """
-        package_dicts = [m.to_dict() for m in self.packages]
-        serialized = json.dumps(package_dicts)
-        return serialized
 
-
-class DebianPackage(object):
+class Package(Model):
     """
     A Pulp object sitting ontop of a deb822 object
     """
-    def __init__(self, data):
-        if isinstance(data, Packages):
-            self._obj = data
+    def __init__(self, deb=None, **kw):
+        if isinstance(deb, Packages):
+            self.data = deb
         else:
-            self._obj = Packages(data)
+            self.data = Packages(kw)
 
-    @staticmethod
-    def lowered_key(key):
-        return key.lower()
-
-    @staticmethod
-    def uppered_key(key):
-        return '-'.join([i.title() for i in key.split("-")])
-
-    @classmethod
-    def from_dict(cls, data):
-        """
-        Parses the given snippet of package metadata into an object
-        representation. This call assumes the JSON has already been parsed
-
-        :return: object representation of the given deb
-        :rtype:  DebianPackage
-        """
-        return cls(data)
-
-    def update_from_dict(self, data):
-        """
-        Updates the instance variables with the values in the given dict.
-        """
-        self._obj.update(data)
-
-    def to_dict(self, full=True):
+    def to_dict(self, full=True, **kw):
         """
         Returns a dict view on the package in the same format as was parsed from
         update_from_dict.
@@ -117,25 +232,17 @@ class DebianPackage(object):
         :return: dict view on the package
         :rtype: dict
         """
-        data = dict([(self.lowered_key(k), v) for k, v in self._obj.items()])
+        data = super(Package, self).to_dict(**kw)
+        data = dict([(self.lowered_key(k), v) for k, v in data.items()])
+
         if not full:
             return data
 
         data.update({
-            'prefix': self.prefix()
+            'prefix': self.prefix(),
+            'filename_short': self.filename().split('/')[-1]
         })
         return data
-
-    def update_from_json(self, json):
-        """
-        Takes the package's metadata in JSON format and merges it into this
-        instance.
-
-        :param json: Package metadata in JSON
-        :type  json: str
-        """
-        parsed = json.loads(json)
-        self.update_from_dict(parsed)
 
     @classmethod
     def from_unit(cls, pulp_unit):
@@ -146,7 +253,7 @@ class DebianPackage(object):
         :type  pulp_unit: pulp.plugins.model.Unit
 
         :return: object representation of the given package
-        :rtype:  DebianPackage
+        :rtype:  Package
         """
         unit_as_dict = copy.copy(pulp_unit.unit_key)
         unit_as_dict.update(pulp_unit.metadata)
@@ -179,7 +286,7 @@ class DebianPackage(object):
         return metadata
 
     def prefix(self):
-        pkg = self._obj.get('package')
+        pkg = self.data.get('package')
         prefix = pkg[0:4] if pkg.startswith('lib') else pkg[0]
         return prefix
 
@@ -193,15 +300,34 @@ class DebianPackage(object):
         return self.filename_from_deb822() or self.filename_from_data()
 
     def filename_from_deb822(self):
-        return self._obj.get('filename')
+        """
+        Get the filename from the self.data
+
+        :return: package standard filename for this package
+        :rtype: str
+        """
+        return self.data.get('filename')
 
     def filename_from_data(self, data={}):
+        """
+        Construct the filename from our own data
+
+        :return: package standard filename for this package
+        :rtype: str
+        """
         data_ = self.to_dict()
         data_.update(data)
         return constants.DEB_FILENAME % data_
 
     def filename_short(self):
+        """
+        :return: Only the filename of this package
+        :rtype: str
+        """
         return self.filename().split('/')[-1]
 
     def key(self):
+        """
+        Get the key representing this package
+        """
         return constants.DEB_KEY % self.to_dict()
--- pulp_deb_common/test/unit/test_common_model.py
+++ pulp_deb_common/test/unit/test_common_model.py
@@ -11,36 +11,11 @@
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
-from debian.deb822 import Packages
-import os.path
 import unittest
+from debian import deb822
 
-from pulp.common.compat import json
-
-from pulp_deb.common import constants
-from pulp_deb.common.model import Repository, DebianPackage
-
-# -- constants ----------------------------------------------------------------
-
-PKG_DATA = {
-    'Architecture': u'amd64',
-    'Conflicts': u'python2.3-crypto, python2.4-crypto',
-    'Depends': u'python2.7, python (>= 2.7.1-0ubuntu2), python (<< 2.8), libc6 (>= 2.14), libgmp10',
-    'Description': u'cryptographic algorithms and protocols for Python\n A collection of cryptographic algorithms and protocols, implemented\n for use from Python. Among the contents of the package:\n .\n * Hash functions: HMAC, MD2, MD4, MD5, RIPEMD160, SHA, SHA256.\n * Block encryption algorithms: AES, ARC2, Blowfish, CAST, DES, Triple-DES.\n * Stream encryption algorithms: ARC4, simple XOR.\n * Public-key algorithms: RSA, DSA, ElGamal.\n * Protocols: All-or-nothing transforms, chaffing/winnowing.\n * Miscellaneous: RFC1751 package for converting 128-key keys\n into a set of English words, primality testing, random number gereration.',
-    'Filename': u'pool/main/p/python-crypto/python-crypto_2.6-2build3~ubuntu12.04.1~grizzly0_amd64.deb',
-    'Installed-Size': u'1500',
-    'MD5sum': u'e9242f4b733d993da0b096bea5809d42',
-    'Maintainer': u'Sebastian Ramacher <s.ramacher@gmx.at>',
-    'Package': u'python-crypto',
-    'Priority': u'optional',
-    'Provides': u'python2.7-crypto',
-    'Replaces': u'python2.3-crypto, python2.4-crypto',
-    'SHA1': u'd40743e0d669b0f6b6499da1892a7a39dbca5f8a',
-    'Section': u'python',
-    'Size': u'362600',
-    'Suggests': u'python-crypto-dbg, python-crypto-doc',
-    'Version': u'2.6-2build3~ubuntu12.04.1~grizzly0',
-    'component': 'main'}
+from pulp_deb.common import constants, samples
+from pulp_deb.common.model import Distribution, Component, Package
 
 
 # -- test cases ---------------------------------------------------------------
@@ -50,47 +25,66 @@ def get_expected(data):
     newdata = {}
     for k, v in data.copy().items():
         if k in constants.PACKAGE_KEYS:
-            newdata[DebianPackage.lowered_key(k)] = v
+            newdata[Package.lowered_key(k)] = v
     return newdata
 
 
-class RepositoryTests(unittest.TestCase):
-    def test_update_from_packages(self):
-        metadata = Repository()
-        path = os.path.dirname(__file__) + "/Packages"
-        metadata.update_from_packages(path)
+PACKAGE = get_expected(samples.data('package'))
+DATA = samples.DATA
+
+import ipdb
 
 
-class DebianPackageTests(unittest.TestCase):
+class DistributionTests(unittest.TestCase):
     def setUp(self):
-        self.deb = DebianPackage(PKG_DATA)
+        self.dist = samples.model('dist')
+
+    def test_serialize_dist_wo_packages(self):
+        self.assertEqual(self.dist.serialize(exclude=['packages']), DATA['dist'])
+
+    def test_serialize_dist_with_packages(self):
+        self.dist.add_package(DATA['component']['name'], DATA['package'])
+
+        dist = self.dist.serialize()
+        self.assertEquals(dist['name'], DATA['dist']['name'])
+
+        cmpts = dist['components']
+        self.assertEquals(len(cmpts), 1)
+        self.assertEquals(cmpts[0]['arch'], DATA['component']['arch'])
+        self.assertEquals(cmpts[0]['name'], DATA['component']['name'])
+        self.assertEquals(len(cmpts[0]['packages']), 1)
+
+
+class ComponentTests(unittest.TestCase):
+    def setUp(self):
+        self.dist = samples.model('dist')
+        ipdb.set_trace()
+
+
+
+class PackageTests(unittest.TestCase):
+    def setUp(self):
+        self.pkg = samples.model('package')
 
     def test_from_dict(self):
-        expected = get_expected(PKG_DATA)
-        self.assertEquals(expected, self.deb.to_dict(full=False))
-
-    def test_to_dict_not_full(self):
-        expected = get_expected(PKG_DATA)
-        self.assertEquals(expected, self.deb.to_dict(full=False))
+        pkg = Package(**samples.load('package'))
+        self.assertEquals(PACKAGE, pkg.to_dict(full=False))
 
     def test_from_deb822(self):
-        expected = get_expected(PKG_DATA)
-        self.assertEquals(expected, self.deb.to_dict(full=False))
+        deb = deb822.Packages(samples.load('package'))
+        pkg = Package(deb=deb)
+        self.assertEquals(PACKAGE, pkg.to_dict(full=False))
+
+    def test_to_dict_not_full(self):
+        self.assertEquals(PACKAGE, self.pkg.to_dict(full=False))
 
     def test_prefix(self):
-        self.assertEquals(self.deb.prefix(), PKG_DATA['Package'][0])
+        self.assertEquals(PACKAGE['package'][0:4], self.pkg.prefix())
 
-    def test_filename_from_url_and_filename(self):
-        deb = DebianPackage(PKG_DATA)
-        deb.filename_from_data()
+    def test_filename_from_data_eq_filename(self):
+        pkg = samples.model('package', component='main')
+        self.assertEquals(pkg.filename_from_data(), pkg.filename_from_deb822())
 
-
-    def test_filename_from_data(self):
-        # FIXME: Need to fix this...
-        return None
-        deb = DebianPackage(PKG_DATA)
-        filename = deb.filename()
-
-        deb_data = deb.to_dict()
-        name = constants.DEB_FILENAME % deb_data
-        self.assertEquals(name, filename)
+    def test_filename_short(self):
+        expected = PACKAGE['filename'].split('/')[-1]
+        self.assertEquals(self.pkg.filename_short(), expected)
